c083d657059eabca766fa0090500f1bfe00e43e9
egiiputra <egiputra450@gmail.com>
2025-03-21 22:31:42 +0700

    creating file in binary route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import * as fs from 'fs';
+import { cwd } from 'process';
+import * as path from 'path';
+import { randomUUID } from 'crypto';
+
+import { Controller, Get, Post, Req, RawBodyRequest } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +14,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('api/v1/binary/slow')
+  uploadBinary(@Req() req: RawBodyRequest<Request>): string {
+
+    fs.open(path.join(cwd(), 'tmp', randomUUID()) , 'ax', function (err, f) {
+      console.log(err);
+      console.log('Saved!');
+    });
+    return 'test binary';
   }
 }
