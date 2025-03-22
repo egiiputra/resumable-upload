4860f5ef495ccf5c95b4ec8f9d7db1842af997f9
egiiputra <egiputra450@gmail.com>
2025-03-22 22:51:15 +0700

    Add file upload handler with multer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import * as fs from 'fs';
 import { cwd } from 'process';
 import * as path from 'path';
 import { randomUUID } from 'crypto';
 
-import { Controller, Get, Post, Req, RawBodyRequest } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
-import { Request } from 'express';
+import { diskStorage } from 'multer';
 
 @Controller()
 export class AppController {
@@ -16,28 +16,18 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post('api/v1/binary/slow')
-  uploadBinary(@Req() req: RawBodyRequest<Request>): string {
-
-    console.log(req.rawBody);
-    // fs.open(path.join(cwd(), 'tmp', randomUUID()) , 'ax', (err, f) => {
-    //   // TODO: replace with error log later
-    //   if (err) throw err;
-    //   fs.write(f, req.rawBody as Buffer, (err, written) => {
-    //     // TODO: replace with error log later
-    //     if (err) throw err;
-
-    //     console.log(`Written ${written} bytes to file.`);
-
-    //     fs.close(f, (err) => {
-    //       // TODO: replace with error log later
-    //       if (err) throw err;
-    //       console.log('File closed.');
-    //     });
-    //   });
-    //   console.log(err);
-    //   console.log('Saved!');
-    // });
-    return 'test binary';
+  @Post('api/v1/form-upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: path.join(cwd(), 'uploads'),
+        filename: (req, file, cb) => {
+          cb(null, randomUUID() + path.extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  uploadBinary(): string {
+    return 'upload file successfully';
   }
 }
