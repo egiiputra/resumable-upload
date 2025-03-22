d09583a1f4f4f84d907483d8a81c1a7976aee82c
egiiputra <egiputra450@gmail.com>
2025-03-22 22:55:37 +0700

    expose uploads folder

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ import * as path from 'path';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    rawBody: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
   await app.listen(process.env.PORT ?? 3000);
 }
