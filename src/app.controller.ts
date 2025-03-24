de7a007b24bd17eb4434b194698a1a9e5277dc8a
egiiputra <egiputra450@gmail.com>
2025-03-24 23:14:54 +0700

    implement checksum basic for uploading file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { cwd } from 'process';
 import * as path from 'path';
 import { randomUUID, createHash } from 'crypto';
+import * as fs from 'fs';
 
 import {
   Controller,
@@ -9,9 +10,11 @@ import {
   Headers,
   UseInterceptors,
   UploadedFile,
+  Res,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -27,16 +30,54 @@ export class AppController {
   uploadBinary(
     @UploadedFile() file: Express.Multer.File,
     @Headers('X-Checksum') checksum: string,
-  ): string {
+    @Res() res: Response,
+  ) {
     const hash = createHash('md5');
 
-    hash.update(file.buffer);
-    if (checksum != hash.copy().digest('hex')) {
-      // TODO: Give bad request response status
-      return 'file corrupt';
-    }
-    // TODO: Store file to disk and give unique name
+    const { buffer, originalname } = file;
 
-    return 'upload file successfully';
+    hash.update(buffer);
+    if (checksum != hash.copy().digest('hex')) {
+      res.status(400).json({
+        message: 'Uploaded file might be corrupted',
+      });
+      return;
+    }
+
+    const filename = path.join(
+      cwd(),
+      'uploads',
+      randomUUID() + path.extname(originalname),
+    );
+
+    fs.open(filename, 'ax', (err, fd) => {
+      if (err) {
+        res.status(500).json({
+          message: 'Upload file failed',
+        });
+        return;
+      }
+
+      fs.write(fd, file.buffer, 0, file.buffer.length, null, (err) => {
+        if (err) {
+          res.status(500).json({
+            message: 'Error writing to file',
+          });
+          return;
+        }
+
+        fs.close(fd, (err) => {
+          if (err) {
+            res.status(500).json({
+              message: 'Error closing file',
+            });
+            return;
+          }
+          res.status(200).json({
+            message: 'File uploaded successfully',
+          });
+        });
+      });
+    });
   }
 }
