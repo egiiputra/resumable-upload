f94c9044ff774cd0ceda068ee5ec40e70f469acd
egiiputra <egiputra450@gmail.com>
2025-04-02 22:18:15 +0700

    fix unit test

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
 
 describe('FilesController', () => {
+  let provider: FilesService;
   let controller: FilesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
+      providers: [FilesService],
     }).compile();
 
+    provider = module.get<FilesService>(FilesService);
     controller = module.get<FilesController>(FilesController);
   });
 
-  it('should be defined', () => {
+  it('provider should be defined', () => {
+    expect(provider).toBeDefined();
+  });
+
+  it('controller should be defined', () => {
     expect(controller).toBeDefined();
   });
 });
