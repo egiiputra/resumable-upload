a9898f53219fbd9087432a4de6dd27c6cd26b873
egiiputra <egiputra450@gmail.com>
2025-05-03 23:12:46 +0700

    Add router for detail uploaded file page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import UploadFile from './pages/UploadFile.jsx'
+import DetailFile from './pages/DetailFile.jsx'
 
 function App() {
 
   return (
-    <UploadFile />
+    <Routes>
+      <Route path="/" element={<UploadFile />} />
+      <Route path="/files/:uuid" element={<DetailFile />} />
+    </Routes>
   )
-
 }
 
 export default App
